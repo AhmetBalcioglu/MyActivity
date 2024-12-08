0e16698027d50f3ce53faa0b3d487d18ece7d231
Ahmet <ahmetbalcioglu2002@hotmail.com>
2024-12-08 11:44:33 +0300

    Added space for images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,18 @@
 @include('inc/header')
 
 <div class="container" style="background-color:#f2a100; margin-top: 100px; height: 800px">
-
     @foreach ($etkinlikler as $etkinlik)
-    <div class="card mb-3">
-        <div class="card-body">
-            <h5 class="card-title"><i><b>{{$etkinlik->etkinlikBaslik}}</b></i></h5>
-            <p class="card-text">{{$etkinlik->etkinlikAciklama}}</p>
+    <div class="row">
+        <div class="col-md-6"></div>
+        <div class="card mb-1 mt-5 col-md-6">
+            <div class="card-body">
+                <h5 class="card-title"><i><b>{{$etkinlik->etkinlikBaslik}}</b></i></h5>
+                <p class="card-text">{{$etkinlik->etkinlikAciklama}}</p>
+            </div>
+        </div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button class="btn btn-primary">Düzenle</button>
+            <button class="btn btn-danger">Sil</button>
         </div>
     </div>
     @endforeach
