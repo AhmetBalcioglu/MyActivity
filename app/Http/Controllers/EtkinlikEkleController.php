89be3d335eaac429d39a5042215a5b2fad3bcb4d
Ahmet <ahmetbalcioglu2002@hotmail.com>
2024-12-08 09:21:57 +0300

    Create Controllers

---
+++ app/Http/Controllers/EtkinlikEkleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EtkinlikEkleController extends Controller
+{
+    public function index(){
+        dd('TODO: Etkinlik ekle');
+    }
+}
