228b86009c40c03c287442b9088ee8de76bdf0c0
Ahmet <ahmetbalcioglu2002@hotmail.com>
2024-12-08 11:02:58 +0300

    Update Controllers

--- app/Http/Controllers/EtkinlikEkleController.php
+++ app/Http/Controllers/EtkinlikEkleController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\etkinlikler;
 use Illuminate\Http\Request;
 
 class EtkinlikEkleController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('etkinlikEkle');
+    }
+
+    public function etkinlikEkle(Request $request)
+    {
+        $etkinliklerDB = new etkinlikler();
+
+        $etkinliklerDB->etkinlikBaslik = $request->etkinlikBaslik;
+        $etkinliklerDB->etkinlikAciklama = $request->etkinlikAciklama;
+        $etkinliklerDB->save();
+
+        return redirect('/');
     }
 }
