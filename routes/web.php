aac6b63f5f66d7fcadfce72bb534251f8e9286e5
Ahmet <ahmetbalcioglu2002@hotmail.com>
2024-12-08 11:57:56 +0300

    Added route to etkinlikSil

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\KonumBilgisiController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
-Route::get('pages/login', [LoginController::class, 'login']);
-Route::get('pages/page-table-listing', [KonumBilgisiController::class, 'index']);
-Route::get('pages/dashboard', [EtkinlikEkleController::class, 'index']);
-Route::post('pages/dashboard', [EtkinlikEkleController::class, 'etkinlikEkle']);
+Route::get('/', [IndexController::class, 'index']); #index page
+Route::get('pages/login', [LoginController::class, 'login']); #login page
+Route::get('pages/page-table-listing', [KonumBilgisiController::class, 'index']); #konum bilgisi page
+Route::get('pages/dashboard', [EtkinlikEkleController::class, 'index']); #etkinlik ekle page
+Route::post('pages/dashboard', [EtkinlikEkleController::class, 'etkinlikEkle']); #etkinlik ekleme islemi
+Route::get('pages/dashboard/{id}', [EtkinlikEkleController::class, 'etkinlikSil']); #etkinlik silme islemi
