20355c6d2eb172b691232f937802401402514a2e
Ahmet <ahmetbalcioglu2002@hotmail.com>
2024-12-08 11:01:03 +0300

    Update index route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
 use App\Http\Controllers\EtkinlikEkleController;
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\KonumBilgisiController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [IndexController::class, 'index']);
 Route::get('pages/login', [LoginController::class, 'login']);
 Route::get('pages/page-table-listing', [KonumBilgisiController::class, 'index']);
 Route::get('pages/dashboard', [EtkinlikEkleController::class, 'index']);
+Route::post('pages/dashboard', [EtkinlikEkleController::class, 'etkinlikEkle']);
