901ce196624f356f6be165c526442b319e179f1e
Ahmet <ahmetbalcioglu2002@hotmail.com>
2024-12-08 09:21:19 +0300

    Add routes for navbar

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\EtkinlikEkleController;
+use App\Http\Controllers\KonumBilgisiController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('pages/login', [LoginController::class, 'login']);
+Route::get('pages/page-table-listing', [KonumBilgisiController::class, 'index']);
+Route::get('pages/dashboard', [EtkinlikEkleController::class, 'index']);
